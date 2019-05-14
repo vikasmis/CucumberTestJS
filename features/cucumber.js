c488d50230674a971d9cf2e947cdbfec15bb5f55
Mishra <vikash.a.mishra@capgemini.com>
2019-05-14 10:33:21 +0200

    first changes to web

---
+++ features/cucumber.js
@@ -0,0 +1,12 @@
+var common = [
+    '--require-module @babel/register',
+    `--format ${
+      process.env.CI || !process.stdout.isTTY ? 'progress' : 'progress-bar'
+    }`,
+    '--format rerun:@rerun.txt',
+    '--format usage:usage.txt',
+  ].join(' ')
+  
+  module.exports = {
+    default: common,
+  }
